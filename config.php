9ab4ded00631bb10582781f9dce9834eb3050991
Hunter Chang <107000218+Wolfieseal@users.noreply.github.com>
2022-06-06 12:40:24 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+	define ( 'FN_API_KEY', '34cc4e90-6b9f-4f6e-bde3-c8da4516dfd7
+')
+?>
---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+
+	include 'config.php';
+
+	function getPlayerStats($platform, $epicNickname) {
+
+		$apiUrlPlayerStatsEndpoint = 'https://api.fortnitetracker.com/v1/profile/' . $platform . '/' . $epicNickname;
+
+		$ch = curl_init()
+
+		curl_setopt( $ch, CURLOPT_URL, $apiUrlPlayerStatsEndpoint)
+
+		curl_setopt ( $ch, CURLOPT_HTTPHEADER, array( 'TRN-Api-Key' . FN_API_KEY));
+
+		curl_setopt ( $ch, CURLOPT_RETURNTRANSFEr, TRUE);
+		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt ( $ch, CURLOPT_SSL_VERIFPEER, FALSE);
+
+		$response = curl_exec ( $ch);
+		curl_close( $ch );
+
+		return json_decode( $response, true)
+		
+	}
+?>
+
+
+
+
+Username #trackerapitest
+Password #Test12345!
